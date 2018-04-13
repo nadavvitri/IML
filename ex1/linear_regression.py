ddcad4e478eff1fe078028971305ce6f834ba45b
nadavvitri <nadav325@gmail.com>
2018-04-13 17:50:43 +0300

    split train and test

--- ex1/linear_regression.py
+++ ex1/linear_regression.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import numpy
-import random
+import numpy as np
+from matplotlib.pyplot import *
 
 
 def pre_processing():
-    data = pd.read_csv("kc_house_data2.csv")
+    data = pd.read_csv("kc_house_data.csv")
 
     # can't be < 0 , e.g like price
     cols_to_clean = ["price", "bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "waterfront", "view",
@@ -13,25 +13,45 @@ def pre_processing():
     cols_to_clean_zeros = ['sqft_living', 'sqft_lot', 'floors', 'sqft_above', 'yr_built', 'sqft_living15', 'sqft_lot15']
     for col in cols_to_clean:
         data[col] = [None if i < 0 else i for i in data[col]]
-    data[cols_to_clean_zeros] = data[cols_to_clean_zeros].replace(0, numpy.NaN)
+    data[cols_to_clean_zeros] = data[cols_to_clean_zeros].replace(0, np.NaN)
     data.dropna(inplace=True)
 
-    y = data.price
-    # remove id, date and price columns
-    data.drop(['id', 'date', 'price'], axis=1, inplace=True)
+    # remove id and date columns
+    data.drop(['id', 'date'], axis=1, inplace=True)
     data = pd.get_dummies(data, columns=["zipcode"], drop_first=True)  # One Hot encoding for zip code
-    return data, y
+    return data
 
 
 def training_data():
-    data, y = pre_processing()
-    w = numpy.linalg.pinv(data).dot(y)
-    size = data.shape[0] / 100
+    train_error, test_error, x_axis = [], [], []
+    data = pre_processing()
     for x in range(1, 100):
-        data = data.sample(frac=1)
-        train_data = data[:x * size]
-        test_data = data[x * size:]
+        x_axis.append(x)
+        # train data set and labels
+        train = data.sample(frac=x/100)
+        y_train = train.price
+        train.drop(['price'], axis=1, inplace=True)
 
+        # test data set and labels
+        test = data.drop(train.index)
+        y_test = test.price
+        test.drop(['price'], axis=1, inplace=True)
+        # model for prediction
+        w = np.matmul(np.transpose(np.linalg.pinv(np.transpose(train))), y_train)
+        # w = np.linalg.pinv(train).dot(y_train)
+        # calculate train error by MSE
+        y_head_train = train.dot(w)
+        train_error.append(np.linalg.norm(y_head_train - y_train))
+        # calculate test error by MSE
+        y_head_test = test.dot(w)
+        test_error.append(np.linalg.norm(y_head_test - y_test))
+
+    suptitle('Graphs', fontweight="bold", fontsize=13)
+    subplot(111)
+    plot(x_axis, test_error, '-')
+    xlabel('x value', fontweight="bold")
+    ylabel('Train error (MSE)', fontweight="bold")
+    show()
 
 
 if __name__ == '__main__':
